90dd1122cab02d4b7c6c42e6e0c511972e1615e7
Sameer Akram <sameerakram3663@gmail.com>
2023-07-14 20:11:19 +0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DevConnector
+# DecConnector
+# DecConnector
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();// used to make short your main source file
+const { check, validationResult } = require('express-validator');
 
-//@route         GET api/users
-//@description   Test route
+//@route         POST api/users
+//@description   Register User
 //@access        Public
 
-router.get('/', (req, res) => {res.send('User route')});
+router.post('/', [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').notEmpty().withMessage('Name is required')
+], (req, res) => {
+    console.log(req.body);
+    res.send('User route')
+
+});
 
 module.exports = router;
